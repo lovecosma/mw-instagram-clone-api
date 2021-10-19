7d6bb9ac96be5a85c1b26161aee30758d625c893
love.cosma <williamsmatthew333@gmail.com>
2021-10-19 14:29:01 -0600

    Added AWS S3.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,30 @@
 class PostsController < ApplicationController
 
+    include Rails.application.routes.url_helpers
+    
+    def index 
+        render json: Post.all
+    end 
+
+
     def create
-        binding.pry
-        post = Post.new(post_params)
+       post = Post.create(caption: post_params["caption"])
+       post.avatar.attach(post_params["file"])
+       post.photo_url = post.avatar.blob.url
+       if post.save 
+            render json: post
+       else
+            render json: post.errors
+       end 
+    end
+    
+    
+
+
+    private 
+
+    def post_params
+        params.permit(:file, :caption)
     end 
 
 end
